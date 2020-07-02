a146372b5d341e96311c78af53e65baa9c06662c
Kelli Jarrell <kelli.jarrell@ninelineapparel.com>
2020-07-01 21:05:27 -0400

    added files

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+create DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+id INT NOT NULL auto_increment,
+burger_name VARCHAR(255) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+PRIMARY KEY (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers VALUES(1, "Bison Burger", False);
+INSERT INTO burgers VALUES(2, "Cheese Burger", True);
+INSERT INTO burgers VALUES(3, "Elk Burger", False);
